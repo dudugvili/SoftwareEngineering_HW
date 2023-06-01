0472929d2339c1aa17d1f89f5862105a869f2add
ShirGivon <Shirgivon4@gmail.com>
2023-06-01 14:10:28 +0300

    This file can be deleted - running example

--- Ex1/MediaPack/Main.java
+++ Ex1/MediaPack/Main.java
@@ -2,7 +2,7 @@ package MediaPack;
 
 public class Main {
     public static void main(String[] args) {
-        // main - Need to go down in the integration class
+        // main - need to go down in the integration class
         MediaApp mediaApp = new MediaApp();
         mediaApp.run();
     }
