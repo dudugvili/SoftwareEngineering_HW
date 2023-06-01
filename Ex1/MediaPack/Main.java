0e5d8ac8f169b9cdbf3fc3d45bd7218ac9398a3c
ShirGivon <Shirgivon4@gmail.com>
2023-06-01 14:09:14 +0300

    add notes

--- Ex1/MediaPack/Main.java
+++ Ex1/MediaPack/Main.java
@@ -2,6 +2,7 @@ package MediaPack;
 
 public class Main {
     public static void main(String[] args) {
+        // main - Need to go down in the integration class
         MediaApp mediaApp = new MediaApp();
         mediaApp.run();
     }
