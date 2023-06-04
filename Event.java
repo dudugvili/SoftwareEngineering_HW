881f2917ccb2947b1b69df4297c388cac9608a73
danihazan <103563034+danihazan@users.noreply.github.com>
2023-06-04 12:28:14 +0300

    Add files via upload
    Created Classes: *Meeting +Occurrence inheriting from Class Event to create the calendar.
    *Class Calendar app 
    updated the phone book to be an independence app (for my use you can use it too)
    Updates in PhoneBook:1.delContact function to remove all contact meetings(using the function of calendarApp)
    2. added new function that finds a contact and returns it - for Calendar App use

---
+++ Event.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class Event {
+
+
+        protected int _duration; // duration of event in minutes
+        protected Date _date; //date of event
+        public Event(){};
+        public Event(Date date,int duration){
+            this._date= (Date) date.clone();
+            this._duration=duration;
+
+        }
+
+        public Date getDate(){return _date;};
+        public  int getDuration(){return  _duration;};
+        public void setDate(Date date){this._date= (Date) date.clone();};
+        public  void setDuration(int duration){this._duration=duration;};
+        public void PrintEvent(){
+         System.out.print("Event Date:"+_date.getDate()+"/"+_date.getMonth()+"/"+_date.getYear()+" ");
+            if(_date.getHours()<10)
+                System.out.print("0");
+            System.out.print(_date.getHours()+":");
+            if(_date.getMinutes()<10)
+                System.out.print("0");
+            System.out.println(_date.getMinutes());
+
+
+
+        //    System.out.print("Event Date:"+_date);
+            System.out.println("Event Duration:"+_duration+" minutes");
+
+
+        }
+    }
+
+
